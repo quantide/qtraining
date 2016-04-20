19552c928b470e96e0a3d7e9429132bd910fe913
Veronica Giro <veronica.giro@quantide.com>
2016-04-20 13:04:32 +0200

    deleted files

--- 050-dplyr-datamanage/output/purl/2-0-verbs.R
+++
@@ -1,5 +0,0 @@
-## ----setup, echo=FALSE, results='hide', message=FALSE--------------------
-library(knitr) 
-options(width=80)
-opts_chunk$set(list(dev = 'png', fig.cap='', fig.show='hold', dpi=100, fig.width=7, fig.height=7, fig.pos='H!'))#, fig.path="figures/lm-"))
-
