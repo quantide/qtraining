b144a42d7f695f785cb829189fae0cac50dd8f92
Veronica Giro <veronica.giro@quantide.com>
2016-04-22 12:02:54 +0200

    modifiche import

--- 010-rbase/output/purl/31-dataimport.R
+++ 010-rbase/output/purl/31-dataimport.R
@@ -1,26 +1,39 @@
-## ---- read.table---------------------------------------------------------
-df = read.table("/home/veronica/dev/qtraining/010-rbase/data/noR/tennis.txt", header = TRUE, sep = "", dec = ".")
+## ----date-requirePackages, echo=FALSE------------------------------------
+# Install required packages
+if(! "XLConnect" %in% installed.packages()) {install.packages("XLConnect")}
+if(! "RSQLite" %in% installed.packages()) {install.packages("RSQLite")}
+###################################################################
+
+## ----read.table, error=TRUE----------------------------------------------
+df = read.table("C:/Users/UserName/Documents/dati/tennis.txt", header = TRUE, sep = "", dec = ".")
+
+## ---- getwd--------------------------------------------------------------
+getwd() 
+
+## ---- setwd, eval=FALSE--------------------------------------------------
+## setwd("C:/Users/UserName/Documents")
+
+## ---- read.table2--------------------------------------------------------
+df = read.table("tennis.txt", header = TRUE)
+
+## ---- head---------------------------------------------------------------
 head(df)
 
-## ---- read.table2, eval=FALSE--------------------------------------------
-## df = read.table("noR/tennis.txt", head = TRUE)
-## df = read.table("noR\\tennis.txt", head = TRUE)
-
 ## ---- nrows1-------------------------------------------------------------
-read.table("noR/tennis.txt", header = TRUE, sep = "", dec = ".", nrows = 3)
+read.table("tennis.txt", header = TRUE, sep = "", dec = ".", nrows = 3)
 
 ## ---- nrows2-------------------------------------------------------------
-read.table("noR/tennis.txt", header = FALSE, sep = "", dec = ".", skip = 2)
+read.table("tennis.txt", header = FALSE, sep = "", dec = ".", skip = 2)
 
 ## ---- nrows3-------------------------------------------------------------
-read.table("noR/tennis.txt", header = FALSE, sep = "", dec = ".", nrows = 2, skip = 2)
+read.table("tennis.txt", header = FALSE, sep = "", dec = ".", nrows = 2, skip = 2)
 
 ## ---- stringsAsFactors---------------------------------------------------
-df = read.table("noR/tennis.txt", header = TRUE, sep = "", dec = ".", stringsAsFactors = FALSE)
+df = read.table("tennis.txt", header = TRUE, sep = "", dec = ".", stringsAsFactors = FALSE)
 head(df)
 
 ## ---- na.strings---------------------------------------------------------
-df = read.table("noR/tennis.NA.txt", header = TRUE, sep = "", dec = ".", na.strings = c("MC", "ND"), stringsAsFactors = FALSE)
+df = read.table("tennis.NA.txt", header = TRUE, sep = "", dec = ".", na.strings = c("MC", "ND"), stringsAsFactors = FALSE)
 head(df)
 
 ## ---- write.table, eval = FALSE------------------------------------------
@@ -28,14 +41,135 @@ head(df)
 ## df = data.frame(a1 = rnorm(10), a2 = rnorm(10), a3 = rnorm(10))
 ## write.table(df, file = "dfWrite.txt")
 
-## ---- RODBCsqlFetch, eval=FALSE------------------------------------------
-## # RODBC driver ought be configured to work properly
-## library("RODBC")
-## conn = odbcConnectExcel("dataManual/dat/weight.xls")
-## sqlTables(conn)$TABLE_NAME
-## df = sqlFetch(conn, "Sheet1")
-## head(df)
-## close(conn)
+## ----use.text, comment=FALSE---------------------------------------------
+require("XLConnect")
+
+## ----outDir_set_up, eval=FALSE-------------------------------------------
+## # Set up output directory and output file name
+## outDir <- "/home/marco/Desktop/xlsx"
+
+## ------------------------------------------------------------------------
+fileXls <- paste(outDir,"newFile.xlsx",sep='/')
+# Delete fileXls if it already exists 
+unlink(fileXls, recursive = FALSE, force = FALSE)
+
+
+## ----new.xlsx, comment=FALSE---------------------------------------------
+exc <- loadWorkbook(filename = fileXls, create = TRUE)
+createSheet(object = exc, name = 'Input')
+saveWorkbook(exc)
+
+## ----g1, echo=FALSE, fig.width=6-----------------------------------------
+include_graphics("images/excel-emptySheet.png")
+
+## ----add.input, comment=FALSE--------------------------------------------
+df <- data.frame('inputType'=c('Day','Month'),'inputValue'=c(1,3))
+writeWorksheet(object = exc, data = df, sheet = "Input", startRow = 1, startCol = 2)
+saveWorkbook(exc)
+
+## ----g2, echo=FALSE, fig.width=3-----------------------------------------
+include_graphics("images/excel-inputSheet.png")
+
+## ----add.airquality, comment=FALSE---------------------------------------
+# Add a sheet named Airquality to exc object
+createSheet(exc,'Airquality')
+saveWorkbook(exc)
+
+## ------------------------------------------------------------------------
+# Add an empty column to airquality dataset before add it to 'Airquality' sheet
+airquality$isCurrent<-NA
+createName(exc, name='Airquality',formula='Airquality!$A$1')
+writeNamedRegion(exc, airquality, name = 'Airquality', header = TRUE)
+saveWorkbook(exc)
+
+## ----g3, echo=FALSE, fig.width=3-----------------------------------------
+include_graphics("images/excel-airqualitySheet.png")
+
+## ----add.formula, comment=FALSE------------------------------------------
+# Define the column index of the cell to edit
+colIndex <- which(names(airquality) == 'isCurrent')
+# Define the excel letter for the column 'Day' and 'Month' needed by the formula 
+letterDay <- idx2col(which(names(airquality) == 'Day'))
+letterMonth <- idx2col(which(names(airquality) == 'Month'))
+
+## ----idx2col1 ,results='markup'------------------------------------------
+letterDay <- idx2col(which(names(airquality) == 'Day'))
+
+## ----idx2col2, echo=FALSE,results='markup'-------------------------------
+cat('letterDay=',letterDay)
+
+## ----apply_formula-------------------------------------------------------
+# Define the formula to apply to the cell
+formulaXls <- paste('IF(AND(',
+                    letterMonth,
+                    2:(nrow(airquality)+1),
+                    '=Input!C3,',
+                    letterDay,
+                    2:(nrow(airquality)+1),
+                    '=Input!C2)',
+                    ',1,0)',sep='')
+setCellFormula(exc, sheet='Airquality', row = 2:(nrow(airquality)+1), col = colIndex, formula = formulaXls)
+saveWorkbook(exc)
+
+## ----g4, echo=FALSE, fig.width=4-----------------------------------------
+include_graphics("images/excel-addFormula.png")
+
+## ----load.xlsx, comment=FALSE--------------------------------------------
+exc2 <- loadWorkbook(fileXls)
+dtAir <- readWorksheet(exc2, 'Airquality')
+
+## ------------------------------------------------------------------------
+createSheet(exc2, name = "OzonePlot")
+createName(exc2, name='OzonePlot',formula='OzonePlot!$A$1')
+saveWorkbook(exc2)
+
+## ----add.plot, comment=FALSE,warning=FALSE-------------------------------
+require(ggplot2)
+# Generate a graph and save it in png format
+fileGraph <- paste(outDir,'graph.png',sep='/')
+png(filename = fileGraph, width = 800, height = 600)
+ozone_plot <- ggplot(dtAir, aes(x=Day, y=Ozone)) + 
+geom_point() + 
+geom_smooth()+
+facet_wrap(~Month, nrow=1)
+print(ozone_plot)
+invisible(dev.off())
+# Add image file created to 'OzonePlot' named region with its original size 
+addImage(exc2, filename =  fileGraph, name = 'OzonePlot', originalSize = TRUE)
+saveWorkbook(exc2)
+
+## ----g5, echo=FALSE, fig.width=6-----------------------------------------
+include_graphics("images/excel-ozonePlot.png")
+
+## ---- message=FALSE------------------------------------------------------
+require(RSQLite)
+
+## ------------------------------------------------------------------------
+con <- dbConnect(RSQLite::SQLite(), "mtcars.sqlite")
+
+## ------------------------------------------------------------------------
+dbWriteTable(con, "mtcars", mtcars)
+
+## ------------------------------------------------------------------------
+dbDisconnect(con)
+
+## ------------------------------------------------------------------------
+con <- dbConnect(RSQLite::SQLite(), "mtcars.sqlite")
+
+## ------------------------------------------------------------------------
+dbListTables(con)
+
+## ------------------------------------------------------------------------
+dbListFields(con, "mtcars")
+
+## ------------------------------------------------------------------------
+dbReadTable(con, "mtcars")
+
+## ------------------------------------------------------------------------
+dbGetQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
+
+## ------------------------------------------------------------------------
+dbDisconnect(con)
 
 ## ---- RODBCsqlQuery, eval=FALSE------------------------------------------
 ## # RODBC driver ought be configured to work properly
