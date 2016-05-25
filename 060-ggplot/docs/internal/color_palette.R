0514759fbdcbf783f44b3fb68462f2b8ca382b3e
Veronica Giro <veronica.giro@quantide.com>
2016-05-25 12:29:51 +0200

    060ggplot: file rmd e r

---
+++ 060-ggplot/docs/internal/color_palette.R
@@ -0,0 +1,10 @@
+par(mar=c(0,4,0,0))
+RColorBrewer::display.brewer.all()
+
+http://colorbrewer2.org/
+  
+  Zeileis, A. K. Hornik, P. Murrell. 2009. Escaping
+RGBland: selecting colors for statistical graphics.
+Computational and Statistics & Data Analysis 
+
+colorspace::choose_palette()
---
+++ 060-ggplot/input/index.md
@@ -0,0 +1,7 @@
+---
+title: "About this course"
+---
+
+![](images/flow.png) 
+
+To be added...
