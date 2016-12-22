42e88058ae12814872141f470360a16742881971
Veronica Giro <veronica.giro@quantide.com>
2016-12-22 15:23:48 +0100

    011-rbase-two-days: ricompilato ok

--- 010-rbase-two-days/output/purl/41-dplyr-intro.R
+++ 010-rbase-two-days/output/purl/41-dplyr-intro.R
@@ -1,19 +1,31 @@
 ## ----message=FALSE-------------------------------------------------------
 require(dplyr)
 
-## ----tbl_df--------------------------------------------------------------
+## ----bank_tbl_df, message=FALSE------------------------------------------
+require(qdata)
+data(bank) 
+bank
+
+## ----pipe1---------------------------------------------------------------
+head(bank)
+
+## ----pipe2---------------------------------------------------------------
+bank %>% head()
+
+## ----pipe3---------------------------------------------------------------
+bank %>% head(n=10)
+
+## ----tbl_df, message=FALSE-----------------------------------------------
 # Example of data frame
-df <- data.frame(matrix(data = runif(20000), nrow = 1000, ncol = 20))
-names(df) <- letters[1:20]
+data("mtcars")
+class(mtcars)
 
-class(df)
-head(df)
-
-# If we do not convert it as a tbl_df, all df rows and columns will be printed when calling df 
-dim(df)
+# If we do not convert it as a tbl_df, all mtcars rows and columns will be printed when calling mtcars 
+dim(mtcars)
+mtcars
 
 # dplyr version of the same data frame (tbl_df conversion)
-dfd <- tbl_df(df)
-class(dfd)
-dfd
+mtcars_tbl <- tbl_df(mtcars)
+class(mtcars_tbl)
+mtcars_tbl
 
